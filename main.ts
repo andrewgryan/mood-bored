f8f176f846a8d9d2b375e8ed155dc83ca265cc83
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-05-24 09:39:27 +0100

    add content-type

--- main.ts
+++ main.ts
@@ -2,5 +2,8 @@ import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
 
 serve(async (_req) => {
   const text = await Deno.readTextFile("index.html");
-  return new Response(text);
+  return new Response(text, {
+    status: 200,
+    headers: { "content-type": "text/html; charset=utf-8" },
+  });
 });
