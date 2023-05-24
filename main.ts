4033512319e69687010858380cd3f7329f8ff719
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-05-24 09:35:58 +0100

    read a static file

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
 
-serve((_req) => new Response("Hello, World!"));
+serve((_req) => {
+  const text = Deno.readTextFile("index.html");
+  return new Response(text);
+});
