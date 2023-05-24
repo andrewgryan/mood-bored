96294d71747003000d6c32d506d94b5feac538a6
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-05-24 09:48:17 +0100

    add a css route

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
 import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
 
-serve(async (_req) => {
+const CSS_ROUTE = new URLPattern({ pathname: "/style.css" });
+
+serve(async (req: Request): Promise<Response> => {
+  const match = CSS_ROUTE.exec(req.url);
+  if (match) {
+    const text = await Deno.readTextFile("style.css");
+    return new Response(text, {
+      status: 200,
+      headers: { "content-type": "text/css; charset=utf-8" },
+    });
+  }
+
   const text = await Deno.readTextFile("index.html");
   return new Response(text, {
     status: 200,
