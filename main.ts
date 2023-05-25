9271f449eb33940003c21b3c8b732979a24c790c
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-05-25 01:10:07 +0100

    add a simple route

--- main.ts
+++ main.ts
@@ -1,14 +1,25 @@
 import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
 
+const BLOG_ROUTE = new URLPattern({ pathname: "/blog/:id" });
 const CSS_ROUTE = new URLPattern({ pathname: "/style.css" });
 
 serve(async (req: Request): Promise<Response> => {
-  const match = CSS_ROUTE.exec(req.url);
+  let match = CSS_ROUTE.exec(req.url);
   if (match) {
     const text = await Deno.readTextFile("style.css");
     return new Response(text, {
       status: 200,
       headers: { "content-type": "text/css; charset=utf-8" },
+    });
+  }
+
+  // Blog post route
+  match = BLOG_ROUTE.exec(req.url);
+  if (match) {
+    const id = match.pathname.groups.id;
+    return new Response(`Blog: ${id}`, {
+      status: 200,
+      headers: { "content-type": "text/plain; charset=utf-8" },
     });
   }
 
