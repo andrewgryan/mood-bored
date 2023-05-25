1d61c0ca4c3bcdaf5e565616589317b49412fabf
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-05-25 23:18:32 +0100

    use ctx.send static file middleware

--- main.ts
+++ main.ts
@@ -3,15 +3,15 @@ import { Application, Router } from "https://deno.land/x/oak@v12.5.0/mod.ts";
 const app = new Application();
 const router = new Router();
 
-router.get("/", async (ctx) => {
-  ctx.response.body = await Deno.readTextFile("index.html");
-  ctx.response.headers.set("content-type", "text/html; charset=utf-8");
-  ctx.response.status = 200;
-});
-router.get("/style.css", async (ctx) => {
-  ctx.response.body = await Deno.readTextFile("style.css");
-  ctx.response.headers.set("content-type", "text/css; charset=utf-8");
-  ctx.response.status = 200;
+app.use(async (ctx, next) => {
+  try {
+    await ctx.send({
+      root: "./src",
+      index: "index.html",
+    });
+  } catch {
+    await next();
+  }
 });
 
 app.use(router.routes());
