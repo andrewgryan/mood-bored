10d903e34fcd293e05bce1a4426226c2b746f1f6
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-05-26 15:04:46 +0100

    add supabase route

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { Application, Router } from "https://deno.land/x/oak@v12.5.0/mod.ts";
 
 const app = new Application();
 const router = new Router();
+
+const supabase = createClient(
+  Deno.env.get("SUPABASE_URL"),
+  Deno.env.get("SUPABASE_KEY")
+);
+
+router.get("/api", async (ctx) => {
+  const { data, error } = await supabase.from("countries").select();
+  ctx.response.body = JSON.stringify({ data, error });
+  ctx.response.headers.set("content-type", "application/json; charset=utf-8");
+  ctx.response.status = 200;
+});
 
 app.use(async (ctx, next) => {
   try {
