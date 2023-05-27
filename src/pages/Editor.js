3127c540d65e429151166a26cc19bd024bbb1e01
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-05-27 21:56:04 +0100

    ship it

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -2,14 +2,8 @@ import van from "https://vanjs.org/code/van-0.11.10.min.js";
 import * as marked from "https://esm.run/marked";
 import DOMPurify from "https://esm.run/dompurify";
 import hljs from "https://esm.run/highlight.js";
-import CountryPicker from "../CountryPicker.js";
 
 const { button, div, textarea } = van.tags;
-
-const addCountry = async (country) => {
-  const { error } = await supabase.from("countries").insert({ name: country });
-  console.log(country, { error });
-};
 
 const Tab = (left, right) => {
   const index = van.state(0);
@@ -50,7 +44,6 @@ const Editor = () => {
   const cls = (flag) => (flag ? "" : "hidden");
   return div(
     Nav(),
-    CountryPicker({ oncountry: addCountry }),
     Tab(
       (visible) => div({ class: cls(visible) }, van.bind(text, toHTML)),
       (visible) =>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
+import van from "https://vanjs.org/code/van-0.11.10.min.js";
 import Nav from "../components/Nav.js";
+import sheet from "./Home.css" assert { type: "css" };
+document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet];
+
+const { div, header, h1, main } = van.tags;
 
 const Home = () => {
-  return Nav();
+  return div({ class: "Home" }, Nav(), main(header(h1("Home page"))));
 };
 
 export default Home;
